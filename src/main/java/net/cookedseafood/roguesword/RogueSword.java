c5516681b988c7d6c204ed03e83d7260c16021c4
CookedSeafood <125453632+CookedSeafood@users.noreply.github.com>
2025-02-16 09:06:53 +0800

    Update to 1.1.0.

--- src/main/java/net/cookedseafood/roguesword/RogueSword.java
+++ src/main/java/net/cookedseafood/roguesword/RogueSword.java
@@ -1,10 +1,16 @@
 package net.cookedseafood.roguesword;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import net.cookedseafood.pentamana.command.ManaCommand;
 import net.cookedseafood.roguesword.command.RogueSwordCommand;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -12,6 +18,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,8 +31,22 @@ public class RogueSword implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	
 	public static final byte VERSION_MAJOR = 1;
-	public static final byte VERSION_MINOR = 0;
-	public static final byte VERSION_PATCH = 4;
+	public static final byte VERSION_MINOR = 1;
+	public static final byte VERSION_PATCH = 0;
+
+	public static final int MANA_CONSUMPTION = 16777216;
+	public static final int STATUS_EFFECT_DURATION = 600;
+	public static final int STATUS_EFFECT_AMPLIFIER = 0;
+	public static final boolean STATUS_EFFECT_AMBIENT = false;
+	public static final boolean STATUS_EFFECT_SHOW_PARTICLES = true;
+	public static final boolean STATUS_EFFECT_SHOW_ICON = true;
+
+	public static int manaConsumption;
+	public static int statusEffectDuration;
+	public static int statusEffectAmplifier;
+	public static boolean statusEffectAmbient;
+	public static boolean statusEffectShowParticles;
+	public static boolean statusEffectShowIcon;
 
 	@Override
 	public void onInitialize() {
@@ -34,6 +55,10 @@ public class RogueSword implements ModInitializer {
 		// Proceed with mild caution.
 
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> RogueSwordCommand.register(dispatcher, registryAccess));
+
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+			RogueSwordCommand.executeReload(server.getCommandSource());
+		});
 
 		UseItemCallback.EVENT.register((player, world, hand) -> {
 			if (Hand.OFF_HAND.equals(hand)) {
@@ -52,7 +77,7 @@ public class RogueSword implements ModInitializer {
 					return ActionResult.PASS;
 				}
 
-				ManaCommand.executeSetManaConsum(source, 16777216);
+				ManaCommand.executeSetManaConsum(source, manaConsumption);
 				if (ManaCommand.executeConsume(source) == 0) {
 					return ActionResult.PASS;
 				}
@@ -60,8 +85,55 @@ public class RogueSword implements ModInitializer {
 				e.printStackTrace();
 			}
 
-			player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 600, 0), player);
+			player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, statusEffectDuration, statusEffectAmplifier, statusEffectAmbient, statusEffectShowParticles, statusEffectShowIcon), player);
 			return ActionResult.PASS;
 		});
+	}
+
+	public static int reload() {
+		String configString;
+		try {
+			configString = FileUtils.readFileToString(new File("./config/roguesword.json"), StandardCharsets.UTF_8);
+		} catch (IOException e) {
+            reset();
+			return 1;
+		}
+
+		JsonObject config = new Gson().fromJson(configString, JsonObject.class);
+
+		manaConsumption =
+			config.has("manaConsumption") ?
+			config.get("manaConsumption").getAsInt() :
+			MANA_CONSUMPTION;
+		statusEffectDuration =
+			config.has("statusEffectDuration") ?
+			config.get("statusEffectDuration").getAsInt() :
+			STATUS_EFFECT_DURATION;
+		statusEffectAmplifier =
+			config.has("statusEffectAmplifier") ?
+			config.get("statusEffectAmplifier").getAsInt() :
+			STATUS_EFFECT_AMPLIFIER;
+		statusEffectAmbient =
+			config.has("statusEffectAmbient") ?
+			config.get("statusEffectAmbient").getAsBoolean() :
+			STATUS_EFFECT_AMBIENT;
+		statusEffectShowParticles =
+			config.has("statusEffectShowParticles") ?
+			config.get("statusEffectShowParticles").getAsBoolean() :
+			STATUS_EFFECT_SHOW_PARTICLES;
+		statusEffectShowIcon =
+			config.has("statusEffectShowIcon") ?
+			config.get("statusEffectShowIcon").getAsBoolean() :
+			STATUS_EFFECT_SHOW_ICON;
+		return 2;
+	}
+
+	public static void reset() {
+		manaConsumption = MANA_CONSUMPTION;
+		statusEffectDuration = STATUS_EFFECT_DURATION;
+		statusEffectAmplifier = STATUS_EFFECT_AMPLIFIER;
+		statusEffectAmbient = STATUS_EFFECT_AMBIENT;
+		statusEffectShowParticles = STATUS_EFFECT_SHOW_PARTICLES;
+		statusEffectShowIcon = STATUS_EFFECT_SHOW_ICON;
 	}
 }
--- src/main/java/net/cookedseafood/roguesword/command/RogueSwordCommand.java
+++ src/main/java/net/cookedseafood/roguesword/command/RogueSwordCommand.java
@@ -1,7 +1,6 @@
 package net.cookedseafood.roguesword.command;
 
 import com.mojang.brigadier.CommandDispatcher;
-
 import net.cookedseafood.roguesword.RogueSword;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.server.command.CommandManager;
@@ -16,11 +15,21 @@ public class RogueSwordCommand {
         dispatcher.register(
             CommandManager.literal("roguesword")
             .then(
+                CommandManager.literal("reload")
+                .requires(source -> source.hasPermissionLevel(2))
+                .executes(context -> executeReload((ServerCommandSource)context.getSource()))
+            )
+            .then(
                 CommandManager.literal("version")
                 .executes(context -> executeVersion((ServerCommandSource)context.getSource()))
             )
         );
     }
+
+    public static int executeReload(ServerCommandSource source) {
+        source.sendFeedback(() -> Text.literal("Reloading RogueSword!"), true);
+        return RogueSword.reload();
+	}
 
     public static int executeVersion(ServerCommandSource source) {
         source.sendFeedback(() -> Text.literal("RogueSword " + RogueSword.VERSION_MAJOR + "." + RogueSword.VERSION_MINOR + "." + RogueSword.VERSION_PATCH), false);
